dfe340918ab05f819c04359c8a063e5684fb4568
KAR16 <kherrera16@gmail.com>
2017-02-09 16:35:38 -0600

    Se logró optimizar el tiempo de carga de la app con el compilador AOT

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
+
 import { InicioPage } from '../pages/inicio/inicio';
 import { Page2 } from '../pages/page2/page2';
 import { PayaescuelaPage } from '../pages/payaescuela/payaescuela';
--- src/app/main.ts
+++ src/app/main.ts
@@ -1,5 +1,13 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+//Bootstrap dinámico con el compilador JIT (Just-in-Time)
+//import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app.module';
+//import { AppModule } from './app.module';
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+//platformBrowserDynamic().bootstrapModule(AppModule);
+
+
+
+//Bootstrap estático con el compilador AOT (Ahead-of-Time)
+import { platformBrowser } from '@angular/platform-browser';
+import { AppModuleNgFactory } from './app.module.ngfactory';
+platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
